a644a170c1d8bacf70f70e710a3f28c7ebbd85bd
fernandomff123 <123@segmail.com>
2026-01-13 14:19:43 +0000

    Instalar o yt-dlp

--- build/10-build.sh
+++ build/10-build.sh
@@ -32,7 +32,7 @@ echo "::endgroup::"
 echo "::group:: Install Packages"
 
 # Install packages using dnf5
-dnf5 install -y google-chrome-stable
+dnf5 install -y yt-dlp
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
