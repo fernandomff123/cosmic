ee761bea2af12e299e5ce92495c864d40fc247f1
fernandomff123 <123@segmail.com>
2026-01-13 14:12:29 +0000

    Instalar o google

--- build/10-build.sh
+++ build/10-build.sh
@@ -32,7 +32,7 @@ echo "::endgroup::"
 echo "::group:: Install Packages"
 
 # Install packages using dnf5
-dnf5 install -y tmux
+dnf5 install -y google-chrome-stable
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
